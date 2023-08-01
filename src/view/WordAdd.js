77385a7870cb02a737c730a330dec74664f6899b
HilalSolak <56636066+HilalSolak@users.noreply.github.com>
2023-08-01 11:29:13 +0300

    Added new warnings for empty input fields. Updated design features

--- src/view/WordAdd.js
+++ src/view/WordAdd.js
@@ -6,10 +6,19 @@ function addWordToDb(event, setWarning) {
     const mayaWord = event.target.mayaWord.value.trim();
     const meaning = event.target.meaning.value.trim();
 
-    if (mayaWord === "") {
-        setWarning("Kelime adı boş olamaz.");
+    if (mayaWord === "" && meaning === "") {
+        setWarning("Kelime ve açıklama kısmını doldurunuz!");
         return;
     }
+    if (mayaWord === "") {
+        setWarning("Kelime girmediniz!");
+        return;
+    }
+    if (meaning === "") {
+        setWarning("Kelime açıklaması boş olamaz!");
+        return;
+    }
+
 
     axios.post("http://localhost:8080/api/words/screen", { mayaWord: mayaWord ,meaning: meaning ,title:"BI"})
         .then(res => {
@@ -30,15 +39,14 @@ function WordAdd() {
     return (
         <>
             <div>
-                <h1>Kelime Ekle</h1>
                 <form onSubmit={(event) => addWordToDb(event, setWarning)}>
                     <label>
                         <input type="text" className="form-control" name="mayaWord" placeholder="Kelime"
                                aria-describedby="basic-addon1"/>
                     </label>
                     <label>
-                        <input style={{marginLeft:10}} type="text" className="form-control" name="meaning" placeholder="Anlamı"
-                               aria-describedby="basic-addon1"/>
+                        <input style={{marginLeft:10}} type="text" className="form-control" name="meaning" placeholder="Açıklama"
+                               aria-describedby="basic-addon2"/>
                     </label>
                     <button style={{marginLeft:20, marginBottom:5}} type="submit" className="btn btn-primary" value="Ekle">EKLE</button>
                 </form>
